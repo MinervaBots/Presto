cbeb97538ebcfb64eb1f174d0eba103097eebe0b
PauloHMattos <paulo.mattos@poli.ufrj.br>
2017-04-23 19:48:11 -0300

    Carnaval

--- Main/Constantes.h
+++ Main/Constantes.h
@@ -2,6 +2,8 @@
 #include "QTRSensors.h"
 #include "Arduino.h"
 #include "math.h"
+#include <IRremote.h>
+
 
 //Constantes Do Robo
 #define RAIO_DAS_RODAS          0.185
@@ -10,27 +12,29 @@
 #define ALTURA_SENSOR           0.120
 #define TENSAO_DE_ALIMENTACAO   10.0
 #define VELOCIDADE_LINEAR       12.0
-unsigned MAX_SPEED = 255; //180
+#define OUT_OF_LINE             100
 
-//float Kps[3] = { 20.25, 20.55 11.8};
-//float Kis[3] = { 0.0023, 0.0023, 0.0015};
-float Kds[3] = { 1.5, 1.5, 1.5};
-//float MAX_SPEEDs = {255, 255,255} ;
-int voltas = 0;
+unsigned MAX_SPEED = 90; //120 170
+unsigned CURVE_SPEED = 70; //100 140
+unsigned SPEED = 120; //200
 
 //Constantes de Controle
-#define KM  7.8164
-float KP =  30.0;//255 com 14.02 volts = 17.8//180 - 13.7//14.0 // antes: 9.0 ; teste malu: 9.8 ; 7.8 140 -> 11.8; 20.20 -> 17,20(14,11)
-float KI = 0.01;//255 com 14.02 volts = 0.002 //0.0 // antes: 0.0015 ; teste malu: 0.0015 
-float KD = 0.8;//255 com 14.02 volts = 1.5 //180 - 2.0 // antes: 0.2 ; teste malu: 0.4
+#define KM  7.8164  //completou: 50 5 0.6, 160 curva
+
+float KP = 40;// 27   // 25 13s -> PID: 26 0.001 0.4 ; 4 sensores ; 200 pwm ; 7.74v 
+float KI = 0;// 13  // 20 0.001 - 100 - 50
+float KD = 0.7;// 0.8 // 0.4 - 0.8 - 0.9
+
 #define DT  0.005
 
 //Pinos do Microcontrolador
 #define BUTTON_PIN  5
-#define R_MOTOR_1 3
-#define R_MOTOR_2 11
-#define L_MOTOR_1 10
-#define L_MOTOR_2 9
+#define R_MOTOR_1 11
+#define R_MOTOR_2 3
+#define L_MOTOR_1 9
+#define L_MOTOR_2 10
+#define SINAL_KILLSWITCH 8
+#define BUZZER 12
 
 // Sensor de reflectancia Center_position {4 sensores = 1500, 6 sensores => 2500, 8 sensores => 3500}
 #define EMITTER_PIN         QTR_NO_EMITTER_PIN
@@ -41,53 +45,35 @@ float KD = 0.8;//255 com 14.02 volts = 1.5 //180 - 2.0 // antes: 0.2 ; teste mal
 
 // Sensores de reflectancia laterais
 #define BORDA_DIREITA 7
-//#define BORDA_ESQUERDA  A5
-// Marcações contando a final
-#define CHEGADA         6
+#define BORDA_ESQUERDA  6
 
 // Variaveis de Debug
-//#define DEBUG
 //#define LOG
 
-#define NUMBER_OF_SAMPLES 100
-
-//Variaveis de Configuracao
-//#define LEAD_LAG
-
-  #ifdef LEAD_LAG
-    #define ZERO
-    #define POLE
-  #endif
-
-#define ALPHA_BETA
-  
-  #ifdef ALPHA_BETA
-    #define ALPHA
-    #define BETA
-  #endif 
-
-#define FIRST_ORDER
-  #ifdef FIRST_ORDER
-    #define ALPHA 0.4
-  #endif
-
-  
+#define NUMBER_OF_SAMPLES 100  
 //Flags
 bool following  = true;
 bool calibrando   = true;
 bool ledstate = false;
+bool canIread = true;
 
 //Variaveis Globais
-int num_marcacoes_pista=6;
+int num_marcacoes_pista=1;
 float erro_maximo   = 10.0; //atan(LARGURA_SENSOR/(2.0 * ALTURA_SENSOR));
 float theta = atan(LARGURA_SENSOR/(2.0 * ALTURA_SENSOR));
 unsigned long pid_last_run  = 0;
+unsigned long last_max_error  = 0;
+float line_error;
+float last_line_error;
 float last_error  = 0.0;
 float output    = 0.0;
 float derivative = 0.0;
 float integral    = 0.0;
 float linear_speed  = VELOCIDADE_LINEAR;  
 
+unsigned long deadline=8400;  // Tempo máximo pro Presto completar a pista
+float ARW=0.0001;
+float BC=0;
 float angular_speed = 0.0;
 float speed_right = 0.0;
 float speed_left  = 0.0;
@@ -112,21 +98,16 @@ unsigned mark2 = 0;
 int contadorSensorDireita=0;
 int contadorSensorEsquerda=0;
 unsigned long read_last_run = 0;
-
-#ifdef LOG
-  float sensors_debug[NUMBER_OF_SAMPLES];
-  int   aquired = 0;
-#endif
-
-#ifdef LEAD_LAG
-  float lead_lag_last_input;
-  float lead_lag_output;
-#endif
+unsigned long read_last_run2 = 0;
+unsigned long stop_time = 0;
+unsigned long start_time = 0;
 
 //Classes
-QTRSensorsAnalog qtra((unsigned char[]) {A5,A4,A3,A2}, NUM_SENSORS, 1);
+//QTRSensorsAnalog qtra((unsigned char[]) {A4,A3,A2,A1}, NUM_SENSORS, 1);
+//QTRSensorsAnalog qtra((unsigned char[]) {A5,A4,A3,A2,A1,A0}, NUM_SENSORS, 1);
+QTRSensorsRC qtra((unsigned char[]) {A4,A3,A2,A1}, NUM_SENSORS,200);
 QTRSensorsRC qtrd((unsigned char[]) {BORDA_DIREITA}, 1, 3000);
-//QTRSensorsAnalog qtre((unsigned char[]) {BORDA_ESQUERDA}, 1, 1);
+QTRSensorsRC qtre((unsigned char[]) {BORDA_ESQUERDA}, 1, 3000);
 Button button = Button(BUTTON_PIN,PULLDOWN);
 
 unsigned int sensors[NUM_SENSORS];
@@ -137,12 +118,19 @@ void calculate_motor_speeds();
 void calculate_pwm();
 void move_robot();
 float pid_control(float error);
+float ARW_pid_control(float error);
 float nonlinear_pid_control(float error);
 float nonlinear2_pid_control (float error);
 float lead_lag_compensator(float signal);
 void store_data(float input);
 void print_data();
 void read_border();
+void check_line();
+
+//Necessarios ao killsiwtch
+IRrecv irrecv(SINAL_KILLSWITCH);
+decode_results frequenciaRecebida;
+void killswitch();
 
 // Funcoes Locomocao 
 void stop() {
@@ -170,15 +158,6 @@ void calculate_motor_speeds()
   Serial.println(speed_right);*/
 }
 
-void calculate_pwm()
-{
-  pwm_right = 255 * abs(speed_right)/(KM * TENSAO_DE_ALIMENTACAO);
-  pwm_right = pwm_right > 255 ? 255 : pwm_right;
-
-  pwm_left = 255 * abs(speed_left)/(KM * TENSAO_DE_ALIMENTACAO);
-  pwm_left = pwm_left > 255 ? 255 : pwm_left;
-}
-
 void move_foward()
 {
   analogWrite(R_MOTOR_1,255);
@@ -187,68 +166,36 @@ void move_foward()
   analogWrite(L_MOTOR_2,0);
 }
 
-void move_robot()
+void move_backward()
 {
-  if(speed_right > 0)
-  {
-    analogWrite(R_MOTOR_1,pwm_right);
-    analogWrite(R_MOTOR_2,0);
-  }
-  else
-  {
-    analogWrite(R_MOTOR_2,pwm_right);
-    analogWrite(R_MOTOR_1,0);
-  }
-
-  if(speed_left > 0)
-  {
-    analogWrite(L_MOTOR_1,pwm_right);
-    analogWrite(L_MOTOR_2,0);
-  }
-  else
-  {
-    analogWrite(L_MOTOR_2,pwm_right);
-    analogWrite(L_MOTOR_1,0);
-  }
+  analogWrite(R_MOTOR_1,0);
+  analogWrite(L_MOTOR_1,0);
+  analogWrite(R_MOTOR_2,255);
+  analogWrite(L_MOTOR_2,255);
 }
 
 void move_robot_old_style(float pid_output)
 {
   if(pid_output > 0){
-    analogWrite(R_MOTOR_1,MAX_SPEED);
+    analogWrite(R_MOTOR_1,SPEED);
     analogWrite(R_MOTOR_2,0);
-    if(MAX_SPEED - pid_output < 0){
+    if(SPEED - pid_output < 0){
       analogWrite(L_MOTOR_1,0);
-      analogWrite(L_MOTOR_2,abs(floor(MAX_SPEED - pid_output)));
+      analogWrite(L_MOTOR_2,abs(floor(SPEED - pid_output)));
     }else{
-      analogWrite(L_MOTOR_1,floor(MAX_SPEED - pid_output));
+      analogWrite(L_MOTOR_1,floor(SPEED - pid_output));
       analogWrite(L_MOTOR_2,0);
     }
- /*   
-    Serial.print("Motor Esquerdo: ");
-    Serial.print(MAX_SPEED);
-    Serial.print("\t");
-    Serial.print("Motor Direito: ");
-    Serial.print(floor(MAX_SPEED - pid_output));
-    Serial.println();
-   */ 
   }else{
-    analogWrite(L_MOTOR_1,MAX_SPEED);
+    analogWrite(L_MOTOR_1,SPEED);
     analogWrite(L_MOTOR_2,0);
-    if(MAX_SPEED + pid_output < 0){
+    if(SPEED + pid_output < 0){
       analogWrite(R_MOTOR_1,0);
-      analogWrite(R_MOTOR_2,floor(MAX_SPEED + pid_output));
+      analogWrite(R_MOTOR_2,floor(SPEED + pid_output));
     }else{
-      analogWrite(R_MOTOR_1,floor(MAX_SPEED + pid_output));
+      analogWrite(R_MOTOR_1,floor(SPEED + pid_output));
       analogWrite(R_MOTOR_2,0);
     }
-/*
-    Serial.print("Motor Esquerdo: ");
-    Serial.print(floor(MAX_SPEED + pid_output));
-    Serial.print("\t");
-    Serial.print("Motor Direito: ");
-    Serial.print(MAX_SPEED);
-    Serial.println();*/
   }
 }
 //Funcoes de Controle
@@ -256,21 +203,49 @@ float pid_control(float error)
 {
   if(millis() - pid_last_run < DT * 1000){
     return output;
-  }
- 
-  //derivative = ALPHA * KD *(error-last_error)/DT + (1.0 - ALPHA) * derivative;
-  
-  integral += KI*DT*error;
+  }  
+  integral = integral + KI*DT*error;
   
   output = KP * error + integral + KD *(error-last_error)/DT;
-  //output = KP*error + integral + KD*(error-last_error);
   last_error = error;
+  
   #ifdef LOG
     store_data(error);
   #endif
+  
   pid_last_run = millis();
   return output;
 }
+
+float ARW_pid_control(float error)
+{
+  if(millis() - pid_last_run < DT * 1000){
+    return output;
+  }
+  
+  if(output>2*SPEED){ //back calculation, ARW=alpha
+    BC=ARW*(output-2*SPEED);
+  }
+  if(output<-2*SPEED){
+    BC=ARW*(output+2*SPEED);
+  }
+  else{
+    BC=0;
+  }
+  
+  integral = integral + (KI*DT*error-BC);
+  
+  output = KP * error + integral + KD *(error-last_error)/DT;
+  last_error = error;
+  
+  #ifdef LOG
+    store_data(error);
+  #endif
+  
+  pid_last_run = millis();
+  return output;
+}
+
 float pid_control_limited(float error)
 {
  if(millis() - pid_last_run < DT * 1000)
@@ -357,69 +332,122 @@ float nonlinear2_pid_control (float error)
   output = KP_nl * error + integral + KD_nl*(error-last_error)/DT;
     
   last_error = error;
+  
   #ifdef LOG
     store_data(error);
   #endif
   return output;
 }
-#ifdef LEAD_LAG
-float lead_lag_compensator(float signal)
-{
-  lead_lag_output = signal - ZERO * lead_lag_last_input + POLE * lead_lag_output;
-  return lead_lag_output;
-}
-#endif
-//Estimadores
-bool on_the_line()
-{
-  for(int i = 0; i < NUM_SENSORS; i++)
-    if(sensors[i] < 500)
-      return 1;
- return 0;
-}
+
 float read_sensors()
 {
-  /*#ifdef DEBUG
+  #ifdef DEBUG
   Serial.print("erro: ");
-  Serial.println(erro_maximo*((qtra.readLine(sensors, QTR_EMITTERS_ON, WHITE_LINE) - CENTER_POSITION)/CENTER_POSITION));
-  #endif*/
+  Serial.print(line_error);
+  Serial.print("\t");
+  Serial.println("");
+  #endif
+
+  line_error = erro_maximo*((qtra.readLine(sensors, QTR_EMITTERS_ON, WHITE_LINE) - CENTER_POSITION)/CENTER_POSITION); // LINHA BRANCA
+  //line_error = erro_maximo*((qtra.readLine(sensors, QTR_EMITTERS_ON) - CENTER_POSITION)/CENTER_POSITION); // LINHA PRETA
   
-  return erro_maximo*((qtra.readLine(sensors, QTR_EMITTERS_ON, WHITE_LINE) - CENTER_POSITION)/CENTER_POSITION);
+  return line_error;
   
   /*if(on_the_line())
     erro = -  *((1000.0-sensors[0]) * 3 + (1000.0-sensors[1]) * 2 + (1000.0 -sensors[2]) - (1000.0-sensors[3]) - (1000-sensors[4]) * 2 - (1000.0-sensors[5]) *3)/12000.0; 
   return erro;*/
 }
 
+float error(float line_error)
+{
+  //funçao para tratar absurdos
+  if( abs(line_error)>2 && (line_error/abs(line_error))!=(last_line_error/abs(last_line_error))){
+    return last_line_error;
+  }
+  else{
+    last_line_error=line_error;
+    return line_error;
+  }
+}
+
+void check_line()
+{
+  if(line_error>9.9 || line_error<-9.9 ){
+    if(millis()-last_max_error > OUT_OF_LINE){
+      following = 0;
+    }
+  }
+  else{
+    last_max_error=millis();
+  }
+}
+
+
 float read_sensors_filtered(float alpha){
   erro = alpha * erro_maximo*((qtra.readLine(sensors, QTR_EMITTERS_ON, WHITE_LINE) - CENTER_POSITION)/CENTER_POSITION) + (1.0 - alpha) * erro;
   return erro;
 }
-void read_border()
+
+void read_right()
 {
   qtrd.readCalibrated(&mark);
+  
   /*#ifdef DEBUG
   Serial.println("Mark: \tContador:");
   Serial.print(mark);
   Serial.print("\t");
   Serial.println(contadorSensorDireita);
   #endif*/
-  if (mark < 100)
+
+  if (mark < 200) // MARCAÇÃO BRANCA // Antes o valor estava 10  27.11.2016
+  //if (mark > 900) // MARCAÇÃO PRETA
   {
-     if( (millis()-read_last_run) > 400){
+     if( (millis()-read_last_run) > 100){
       contadorSensorDireita = contadorSensorDireita + 1;
+      digitalWrite(13,(contadorSensorDireita%2));
       read_last_run=millis();
      }
   }
 }
-void alpha_beta_filter()
-{
 
-}
-void first_order()
+void read_left()
 {
-
+  qtre.readCalibrated(&mark2);
+  
+  /*#ifdef DEBUG
+  Serial.println("Mark: \tContador:");
+  Serial.print(mark2);
+  Serial.print("\t");
+  Serial.println(contadorSensorEsquerda);
+  #endif*/
+  
+  if (mark2 < 100)
+  {
+     if( (millis()-read_last_run2) > 120){
+      contadorSensorEsquerda = contadorSensorEsquerda + 1;
+      if(contadorSensorEsquerda==8){
+        KP=40;
+      }
+      else{
+        KP=33.5;
+      }
+      read_last_run2=millis();
+     }
+  }
 }
+
+//Função que troca o valor da variável following caso o botão do meio do controle da Sky da equipe seja apertado
+void killswitch()
+{
+  if(irrecv.decode(&frequenciaRecebida)){ 
+    if(frequenciaRecebida.value == 1336)
+    {
+      following = 0;
+    }
+    irrecv.resume(); // Receive the next value
+  }
+}
+
 // Funcoes de Aquisicao de dados
 #ifdef LOG
 void store_data(float input)
@@ -441,23 +469,24 @@ void print_each_sensor()
     Serial.print(sensors[i]);
     Serial.print("\t");
   }
-  Serial.print("\n");
+  Serial.print("\t");
+  //Serial.print("\n");
 }
 void print_all()
 {
   Serial.print(millis());
   Serial.print(" Erro =");
   Serial.print(last_error);
-  Serial.print(" Angular Speed ");
-  Serial.print(angular_speed);
+  //Serial.print(" Angular Speed ");
+  //Serial.print(angular_speed);
   Serial.print(" PWMs ");
   Serial.print(pwm_right);
   Serial.print(" ");
   Serial.print(pwm_left);
-  Serial.print(" Velocidades ");
-  Serial.print(speed_right);
-  Serial.print(" ");
-  Serial.println(speed_left); 
+  //Serial.print(" Velocidades ");
+  //Serial.print(speed_right);
+  //Serial.print(" ");
+  //Serial.println(speed_left); 
 }
 void print_time()
 {
