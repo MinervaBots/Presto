1f97b67d8cf7501e034a890a99242c6a644d7cf4
PauloHMattos <paulo.mattos@poli.ufrj.br>
2017-05-01 14:48:41 -0300

    Completa a implementação de Position

--- Main/lib/Position/Position.cpp
+++ Main/lib/Position/Position.cpp
@@ -1,1 +1,22 @@
 #include "Position.hpp"
+#include <math.h>
+
+Position::Position(float x, float y, float heading)
+{
+  setX(x);
+  setY(y);
+  setHeading(heading);
+}
+
+float Position::distance(Position position1, Position position2)
+{
+  return sqrt(pow((position1.getX() - position2.getX()), 2) + pow(position1.getY() - position2.getY(), 2));
+}
+
+Position Position::gap(Position position1, Position position2)
+{
+  float gapX = position2.getX() - position1.getX();
+	float gapY = position2.getY() - position1.getY();
+	float gapHeading = position2.getHeading() - position1.getHeading();
+	return Position(gapX, gapY, gapHeading);
+}
--- Main/lib/Position/Position.hpp
+++ Main/lib/Position/Position.hpp
@@ -4,6 +4,8 @@
 class Position
 {
 public:
+  Position() : Position(0, 0, 0) {}
+  Position(float x, float y, float heading);
   float getX() { return m_X; }
   float getY() { return m_Y; }
   float getHeading() { return m_Heading; }
@@ -11,7 +13,9 @@ public:
   void setX(float x) { m_X = x; }
   void setY(float y) { m_Y = y; }
   void setHeading(float heading) { m_Heading = heading; }
-  
+
+  static float distance(Position position1, Position position2);
+  static Position gap(Position position1, Position position2);
 private:
   float m_X;
   float m_Y;
