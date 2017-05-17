6c4f8a7f83cb8d11b5366f5b6dae43b0fbec0c78
PauloHMattos <paulo.mattos@poli.ufrj.br>
2017-05-17 00:27:50 -0300

    Pinos do IR para o kilswitch
    Agora os pinos estão de acordo com a eletrônica do Presto

--- ATtiny/Presto_ATtiny_IR/Presto_ATtiny_IR.ino
+++ ATtiny/Presto_ATtiny_IR/Presto_ATtiny_IR.ino
@@ -1,7 +1,7 @@
 #include "IRremote.h"
 
-#define IR_INPUT_PIN 0
-#define IR_OUTPUT_PIN 1
+#define IR_INPUT_PIN A2
+#define IR_OUTPUT_PIN 0
 #define SIGNAL "adfasdf"
 
 
@@ -10,7 +10,6 @@ decode_results results;
 
 void setup()
 {
-  pinMode(IR_INPUT_PIN, INPUT);
   pinMode(IR_OUTPUT_PIN, OUTPUT);
   irrecv.enableIRIn();
 }
@@ -19,7 +18,7 @@ void loop()
 {
   if (irrecv.decode(&results))
   {
-    if(results.value == SIGNAL)
+    //if(results.value == SIGNAL)
     {
       digitalWrite(IR_OUTPUT_PIN, HIGH);
     }
